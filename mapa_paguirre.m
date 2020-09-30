c0088f4d00c4cf3800e76aa8e20557c5eb8010c7
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2020-09-30 16:50:04 -0300

    Update mapa_paguirre.m

--- mapa_paguirre.m
+++ mapa_paguirre.m
@@ -1,5 +1,6 @@
 % 
-% Adapted from https://www.eoas.ubc.ca/~rich/map.html#9._Zoom_in_on_Prince_Edward_Island_to_co
+%  20200930 - Andres Sepulveda (andres.sepulveda@gmail.com)
+%        Adapted from https://www.eoas.ubc.ca/~rich/map.html#9._Zoom_in_on_Prince_Edward_Island_to_co
 %
 % Missing: Add lines between corners of an area and a central point in the other area
 %
@@ -10,24 +11,23 @@ win_start
     m_coast('patch',[0 1 0]);
     m_grid('linestyle','none','linewidth',2,'tickdir','out',...
            'xaxisloc','top','yaxisloc','right','fontsize',6);
-%    m_text(-69,51,'Standard coastline','color','r','fontweight','bold');
-%    m_ruler([.5 .9],.8,3,'fontsize',8)
+    m_line(-73.525,-45.155,'marker','*','markersize',8,'color','r')
     
     axes('position',[.01 .1 .5 .7]);
     m_proj('albers equal-area','lat',[-41 -47],'long',[-76 -72],'rect','on');
     m_gshhs_h('patch',[.2 .8 .2]);
     m_grid('linestyle','none','linewidth',2,'tickdir','out',...
            'xaxisloc','top','fontsize',6);
-%    m_text(-80,52.5,'GSHHS\_C (crude)','color','m','fontweight','bold');
-%    m_ruler([.5 .9],.8,2,'fontsize',8);
-           
+    m_line(-73.525,-45.155,'marker','*','markersize',8,'color','r')
+    
     axes('position',[.45 .05 .47 .47]);
     m_proj('albers equal-area','lat',[-45.18 -45.12],'long',[-73.56 -73.48],'rect','on');
     m_gshhs_f('patch',[.7 .9 .7]);
     m_grid('linestyle','none','linewidth',2,'tickdir','out',...
            'xaxisloc','top','yaxisloc','right','fontsize',6);
-%    m_text(-63.95,46.56,'GSHHS\_F (full)','color','m','fontweight','bold');
     m_ruler([.2 .7],.95,4);
-
-%    axes('position',[.1 .1 .9 .9]);
-%    line([.3 .5],[.7 .6])
+    m_line(-73.525,-45.155,'marker','*','markersize',8,'color','r')
+    
+    m_text(-73.565,-45.187,'Las Huichas Island','color','k','fontweight','bold','fontsize',14);
+    m_text(-73.575,-45.188,'*','color','r','fontweight','bold','fontsize',28);
+    
